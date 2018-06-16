3a98b593cb3c72762b3dc13a162869cfa4e9e0be
jo-qzy <2651933495@qq.com>
2018-06-16 11:09:57 +0800

    内联，const，友元，静态变量的使用

--- InlineConstFriendAndStatic.cpp
+++ InlineConstFriendAndStatic.cpp
@@ -9,7 +9,9 @@ public:
 		:_year(year)
 		,_month(month)
 		,_day(day)
-	{}
+	{
+		count++;//统计构造函数调用次数
+	}
 
 	void showDate()
 	{
@@ -18,6 +20,7 @@ public:
 
 	void showDate()const
 	{
+		//const修饰函数，当有const变量传入，会自动将this指针变为const
 		cout << _year << "-" << _month << "-" << _day << endl;
 	}
 
@@ -26,18 +29,42 @@ public:
 		//在类内的函数默认为内联函数，所以在这里写inline和不写没有区别
 		return;//什么也不做就是说明一下
 	}
+	
+	ostream& operator<<(ostream& out)
+	{
+		//重载<<，这个输出实在是太奇怪了，还是用友元吧
+		out << _year << "-" << _month << "-" << _day << endl;
+		return out;
+	}
 
 	friend void setDate(Date& d);
+	friend ostream& operator<<(ostream& out, Date& d);
+
+	static void printCount()
+	{
+		cout << count << endl;
+	}
 private:
 	int _year;
 	int _month;
 	int _day;
+	
+	static int count;
 };
 
 void setDate(Date& d)
 {
 	d._year = 2000;
 }
+
+ostream& operator<<(ostream& out, Date& d)
+{
+	//友元函数的输出重载
+	out << d._year << "-" << d._month << "-" << d._day << endl;
+	return out;
+}
+
+int Date::count = 0;
 
 int main()
 {
@@ -47,6 +74,12 @@ int main()
 	d2.showDate();//const修饰的类调const修饰的函数
 	
 	setDate(d1);//友元函数
+	
+	d1 << cout;//类内重载，用起来怪怪的
+	cout << d1;//友元函数重载<<，正常多了
+	
+	Date::printCount();//输出一下静态变量，这里的静态变量统计的是构造函数调用的次数
+	
 	system("pause");
 	return 0;
 }
